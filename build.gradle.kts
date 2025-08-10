da06a4d67d307f0070ac964dc4f5757ee0caeb34
M Hata <8550781+TaboHoto@users.noreply.github.com>
2025-08-10 19:50:02 +0900

    indent

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,26 @@
 //gradle publishToMavenLocal
 defaultTasks("jar")
 plugins{
-  id("java-library")
-  id("maven-publish")
+ id("java-library")
+ id("maven-publish")
 }
 group = "tabou"
 version = "0.1"
-repositories.mavenCentral()
-sourceSets {
-    main {
-        java.srcDir("src")
-    }
+sourceSets{
+ main{
+  java.srcDir("src")
+ }
 }
 tasks.withType<JavaCompile>().configureEach{
   options.encoding = "UTF-8"
 }
-
+tasks.withType<Jar>().configureEach{
+ archiveBaseName.set(project.group.toString() + "."
+  + rootProject.name)
+}
 publishing {
  publications {
-  create<MavenPublication>("maven") {
+  create<MavenPublication>("maven"){
    from(components["java"])
    artifactId = (project.group.toString() + "."
     + rootProject.name)
